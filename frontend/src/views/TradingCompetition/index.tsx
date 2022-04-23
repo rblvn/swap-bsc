8484c3104817e235bcab4fa93826726b3e990092
Ivan <sugudushka@gmail.com>
2022-04-23 15:51:36 +0300

    it works
    **but only with pancakeswap pools

--- frontend/src/views/TradingCompetition/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import MoboxCompetition from './MoboxCompetition'
-
-const TradingCompetitionPage: React.FC = () => {
-  // TODO: Add <SubMenu /> add historical pages
-  return <MoboxCompetition />
-}
-
-export default TradingCompetitionPage
